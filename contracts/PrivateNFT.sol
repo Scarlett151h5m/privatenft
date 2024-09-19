4998376df8cdb64385880fac9b01fba1211ac994
Scarlett151h5m <Scarlett151h5m@hotmail.com>
2024-09-19 06:38:53 +0000

    Initialised

--- contracts/PrivateNFT.sol
+++ contracts/PrivateNFT.sol
@@ -8,7 +8,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract PrivateNFT is ERC721, ERC721Burnable, Ownable {
     constructor(address initialOwner)
-        ERC721("jj","jj")
+        ERC721("uu","uu")
         Ownable(initialOwner)
     {}
 
